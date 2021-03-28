83914869ab9846d20807a7a5c1ed852b7a602984
Chloe Uy <chloeuy@ucla.edu>
2021-03-28 03:07:27 -0700

    updating current site for other sites

--- otherfiles.js
+++ otherfiles.js
@@ -1,6 +1,19 @@
 var storageCnt = localStorage.length
 
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+    let url = tabs[0].url; 
+ 
+   document.body.innerHTML += '<div class="c"><div class="current-link-list">current site: </div><div class="group-current">' + url + '</div><button class="openbutton-current">open</button></div>'
+   
+  });
+ 
+
 for (var i=0; i<storageCnt; i++)
 {
     document.body.innerHTML += '<div class="c"><div class="group">' + localStorage.key(i) + '</div><button class="openbutton">open</button><button class="deletebutton">delete</button>'
 }
+
+
+ 
+ 
+ 
