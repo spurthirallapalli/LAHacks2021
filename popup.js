a5669ec1a41a469733b7cb1db92f4b2ebd11ecea
Spurthi <spurthi.rallapalli@gmail.com>
2021-03-28 00:07:53 -0700

    hahahaha

--- popup.js
+++ popup.js
@@ -1,10 +1,3 @@
-document.getElementById("yes").addEventListener("click", myFunction);
-
-function myFunction() {
-    console.log("Button yes pushed!")
-
-}
-
 document.getElementById("no").addEventListener("click", myFunctionTwo);
 
 function myFunctionTwo() {
