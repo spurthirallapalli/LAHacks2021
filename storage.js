82307ebbf9692c14f656204a7b7d5d44b4865877
Chloe Uy <chloeuy@ucla.edu>
2021-03-28 02:56:15 -0700

    adding links to others button

--- storage.js
+++ storage.js
@@ -1,10 +1,8 @@
 var el = document.getElementById('lbutton');
 if(el){
-    
     var myKey; 
     chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
         myKey = tabs[0].url; 
-        
     });
     
     document.getElementById("lbutton").addEventListener("click", function ()
@@ -15,4 +13,4 @@ if(el){
             console.log("note saved");
         } , true);
 }
- 
+
