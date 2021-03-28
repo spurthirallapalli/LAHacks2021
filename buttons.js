292918c9685fabb60b2afe265d6b0801e0b30cea
jpandh <jasanpreet@g.ucla.edu>
2021-03-27 23:05:45 -0700

    updating files

---
+++ buttons.js
@@ -0,0 +1,13 @@
+document.getElementById("yes").addEventListener("click", myFunction);
+
+function myFunction() {
+    alert("Button yes pushed!")
+
+}
+
+document.getElementById("no").addEventListener("click", myFunctionTwo);
+
+function myFunctionTwo() {
+    alert("Button no pushed!")
+
+}
--- popup.js
+++ popup.js
@@ -1,18 +1,15 @@
-// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-//     document.write(`<h3>The tabs you're on are:</h3>`);
-//     document.write('<ul>');
-//     for (let i = 0; i < tabs.length; i++) {
-//         document.write(`<li>${tabs[i].url}</li>`);
-//     }
-//     document.write('</ul>');
-// });
-// document.getElementById("vbutton").addEventListener("click", function() {
-//     alert("Hello World") ;
-// });
+var listOfTabs = []; 
 
-var el = document.getElementById('vbutton');
-if(el){
-  el.addEventListener("click", function() {
-    console.log("Hello World") ;
-})
-};
+chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+    document.write(`<h3>The tabs you're on are:</h3>`);
+    // document.write('<ul>');
+    for (let i = 0; i < tabs.length; i++) {
+        // document.write(`<li>${tabs[i].url}</li>`);
+        listOfTabs.push(tabs[i].url);
+    }
+    // document.write('</ul>');
+    // chrome.tabs.update( tabs[0].id, { url: "http://stackoverflow.com//" } ); 
+    // document.write(`<li>${listOfTabs}</li>`);
+});
+
+
