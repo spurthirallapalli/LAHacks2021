0848474f58017c6d232602e3d0272deef721ccad
jpandh <jasanpreet@g.ucla.edu>
2021-03-28 03:27:27 -0700

    updated files

--- otherfiles.js
+++ otherfiles.js
@@ -13,7 +13,3 @@ for (var i=0; i<storageCnt; i++)
     document.body.innerHTML += '<div class="c"><div class="group">' + localStorage.key(i) + '</div><button class="openbutton">open</button><button class="deletebutton">delete</button>'
 }
 
-
- 
- 
- 
