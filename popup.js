8686417288d5077e217d3c8c0740f395f2a69572
jpandh <jasanpreet@g.ucla.edu>
2021-03-27 21:29:39 -0700

    included event listener

--- popup.js
+++ popup.js
@@ -1,8 +1,18 @@
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    document.write(`<h3>The tabs you're on are:</h3>`);
-    document.write('<ul>');
-    for (let i = 0; i < tabs.length; i++) {
-        document.write(`<li>${tabs[i].url}</li>`);
-    }
-    document.write('</ul>');
-});
+// chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
+//     document.write(`<h3>The tabs you're on are:</h3>`);
+//     document.write('<ul>');
+//     for (let i = 0; i < tabs.length; i++) {
+//         document.write(`<li>${tabs[i].url}</li>`);
+//     }
+//     document.write('</ul>');
+// });
+// document.getElementById("vbutton").addEventListener("click", function() {
+//     alert("Hello World") ;
+// });
+
+var el = document.getElementById('vbutton');
+if(el){
+  el.addEventListener("click", function() {
+    console.log("Hello World") ;
+})
+};
