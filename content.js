9f099021f130265fc368fd345241c415d9a600b3
Chloe Uy <chloeuy@ucla.edu>
2021-03-27 22:49:42 -0700

    sync current URL name with frontend

--- content.js
+++ content.js
@@ -33,13 +33,14 @@
 // chrome.tabs.getCurrent(function(tab){
 //     alert(tab.url);
 // });
-
+/*
 chrome.tabs.query({active: true, lastFocusedWindow: true}, function(tabs) {
     // and use that tab to fill in out title and url
     var tab = tabs[0];
     console.log(tab.url);
     alert(tab.url);
 });
+*/
 
 // chrome.runtime.sendMessage({ text: "what is my tab_id?" }, tabId => {
 //     console.log('My tabId is', tabId);
--- popup.js
+++ popup.js
@@ -1,8 +1,15 @@
-chrome.tabs.query({windowId: chrome.windows.WINDOW_ID_CURRENT}, (tabs) => {
-    document.write(`<h3>The tabs you're on are:</h3>`);
-    document.write('<ul>');
-    for (let i = 0; i < tabs.length; i++) {
-        document.write(`<li>${tabs[i].url}</li>`);
-    }
-    document.write('</ul>');
-});
+
+document.getElementById("yes").addEventListener("click", myFunction);
+
+function myFunction() {
+    console.log("Button yes pushed!")
+
+}
+
+document.getElementById("no").addEventListener("click", myFunctionTwo);
+
+function myFunctionTwo() {
+    console.log("Button no pushed!")
+
+}
+
